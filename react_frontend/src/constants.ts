64972ea9293975d81f2af60774c8ea043d2de9de
Razvan Moga <61563457+RazvanAndreiMoga@users.noreply.github.com>
2023-04-09 17:22:18 +0300

    Update constants.ts

--- react_frontend/src/constants.ts
+++ react_frontend/src/constants.ts
@@ -1,1 +1,2 @@
-export  const BACKEND_API_URL = "http://localhost:8000";
+// export  const BACKEND_API_URL = "http://localhost:8000";
+export  const BACKEND_API_URL = "http://ec2-13-48-126-169.eu-north-1.compute.amazonaws.com/";
