71a4b33ea14a843847e32a8fa58eaac01f5ca5d6
914-Razvan-Moga <61563457+RazvanAndreiMoga@users.noreply.github.com>
2023-04-11 20:09:41 +0300

    changed backend url

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,4 @@
-export  const BACKEND_API_URL = "http://localhost:8000";
+const PROD_BACKEND_API_URL = "https://rzv-sdi.mooo.com";
+const DEV_BACKEND_API_URL = "http://127.0.0.1:8000";
+
+export const BACKEND_API_URL = process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
