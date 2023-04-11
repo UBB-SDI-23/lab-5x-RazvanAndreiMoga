1eb4ca3b6b53df267ecfbf11442ebedb45d4fca5
914-Razvan-Moga <61563457+RazvanAndreiMoga@users.noreply.github.com>
2023-04-11 20:12:52 +0300

    populate script python

--- populate_data.py
+++ populate_data.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     from faker import Faker
 
     fake = Faker()
-    n = 10000
+    n = 1000000
     print("Creating data")
     for _ in range(n):
         Teacher.objects.create(name=fake.name(), email=fake.email(), address=fake.address(), phone=fake.phone_number(),
@@ -20,6 +20,8 @@ if __name__ == '__main__':
         Course.objects.create(name=fake.name(), description=fake.text(),
                               teacher=Teacher.objects.last(),
                               credits=fake.random_int(min=1, max=10), exam_date=fake.date())
+    n = 10000000
+    for _ in range(n):
         CourseStudent.objects.create(course=Course.objects.last(),
                                      student=Student.objects.last(),
                                      exam_date=fake.date(), grade=fake.random_int(min=1, max=10))
