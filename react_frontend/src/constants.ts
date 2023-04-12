b086c90265615262e6ee80b89cdaa43d48049954
Razvan Moga <61563457+RazvanAndreiMoga@users.noreply.github.com>
2023-04-12 12:14:00 +0300

    Update constants.ts

--- react_frontend/src/constants.ts
+++ react_frontend/src/constants.ts
@@ -1,4 +1,5 @@
-const PROD_BACKEND_API_URL = "https://rzv-sdi.mooo.com";
-const DEV_BACKEND_API_URL = "http://127.0.0.1:8000";
+// const PROD_BACKEND_API_URL = "https://rzv-sdi.mooo.com";
+// const DEV_BACKEND_API_URL = "http://127.0.0.1:8000";
 
-export const BACKEND_API_URL = process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
+// export const BACKEND_API_URL = process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
+export const BACKEND_API_URL = "http://127.0.0.1:8000";
