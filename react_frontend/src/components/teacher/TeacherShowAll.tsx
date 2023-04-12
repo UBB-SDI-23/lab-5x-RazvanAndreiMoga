d810f3aef180c8a729842cada33dd92cafd45e89
Razvan Moga <61563457+RazvanAndreiMoga@users.noreply.github.com>
2023-04-12 12:11:21 +0300

    Update TeacherShowAll.tsx

--- react_frontend/src/components/teacher/TeacherShowAll.tsx
+++ react_frontend/src/components/teacher/TeacherShowAll.tsx
@@ -42,6 +42,19 @@ export const AllTeachers = () => {
           });
   }, []);
 
+  const handleSort = (key: keyof Teacher) => {
+    const sortedTeachers = [...teachers].sort((a, b) => {
+      if ((a[key] as any) === undefined || (b[key] as any) === undefined) return 0;
+      if ((a[key] as any) < (b[key] as any)) return -1;
+      if ((a[key] as any) > (b[key] as any)) return 1;
+      return 0;
+    });
+    setTeachers(sortedTeachers);
+  };
+  
+  
+
+
   return (
       <Container>
           <h1 style={{ margin: "100px 0 30px 0" }}>All teachers</h1>
@@ -55,54 +68,122 @@ export const AllTeachers = () => {
               </IconButton>
           )}
           {!loading && teachers.length > 0 && (
-              <TableContainer component={Paper}>
-                  <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                      <TableHead>
-                          <TableRow>
-                              <TableCell>#</TableCell>
-                              <TableCell align="center">Name</TableCell>
-                              <TableCell align="center">Address</TableCell>
-                              <TableCell align="center">Phone</TableCell>
-                              <TableCell align="center">Age</TableCell>
-                              <TableCell align="center">Email</TableCell>
-                          </TableRow>
-                      </TableHead>
-                      <TableBody>
-                          {teachers.map((teacher, index) => (
-                              <TableRow key={teacher.id}>
-                                  <TableCell component="th" scope="row">
-                                      {index + 1}
-                                  </TableCell>
-                                  <TableCell align="center">{teacher.name}</TableCell>
-                                  <TableCell align="center">{teacher.address}</TableCell>
-                                  <TableCell align="center">{teacher.phone}</TableCell>
-                                  <TableCell align="center">{teacher.age}</TableCell>
-                                  <TableCell align="center">{teacher.email}</TableCell>
+            //   <TableContainer component={Paper}>
+            //       <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            //           <TableHead>
+            //               <TableRow>
+            //                   <TableCell>#</TableCell>
+            //                   <TableCell align="center">Name</TableCell>
+            //                   <TableCell align="center">Address</TableCell>
+            //                   <TableCell align="center">Phone</TableCell>
+            //                   <TableCell align="center">Age</TableCell>
+            //                   <TableCell align="center">Email</TableCell>
+            //               </TableRow>
+            //           </TableHead>
+            //           <TableBody>
+            //               {teachers.map((teacher, index) => (
+            //                   <TableRow key={teacher.id}>
+            //                       <TableCell component="th" scope="row">
+            //                           {index + 1}
+            //                       </TableCell>
+            //                       <TableCell align="center">{teacher.name}</TableCell>
+            //                       <TableCell align="center">{teacher.address}</TableCell>
+            //                       <TableCell align="center">{teacher.phone}</TableCell>
+            //                       <TableCell align="center">{teacher.age}</TableCell>
+            //                       <TableCell align="center">{teacher.email}</TableCell>
                                   
-                                  <TableCell align="center">
-                                      <IconButton
-                                          component={Link}
-                                          sx={{ mr: 3 }}
-                                          to={`/teachers/${teacher.id}/details`}>
-                                          <Tooltip title="View teacher details" arrow>
-                                              <ReadMoreIcon color="primary" />
-                                          </Tooltip>
-                                      </IconButton>
+            //                       <TableCell align="center">
+            //                           <IconButton
+            //                               component={Link}
+            //                               sx={{ mr: 3 }}
+            //                               to={`/teachers/${teacher.id}/details`}>
+            //                               <Tooltip title="View teacher details" arrow>
+            //                                   <ReadMoreIcon color="primary" />
+            //                               </Tooltip>
+            //                           </IconButton>
 
-                                      <IconButton component={Link} sx={{ mr: 3 }} to={`/teachers/${teacher.id}/edit`}>
-                                          <EditIcon />
-                                      </IconButton>
+            //                           <IconButton component={Link} sx={{ mr: 3 }} to={`/teachers/${teacher.id}/edit`}>
+            //                               <EditIcon />
+            //                           </IconButton>
 
-                                      <IconButton component={Link} sx={{ mr: 3 }} to={`/teachers/${teacher.id}/delete`} >
-                                          <DeleteForeverIcon sx={{ color: "red" }} />
-                                      </IconButton>
-                                  </TableCell>
-                              </TableRow>
-                          ))}
-                      </TableBody>
-                  </Table>
-              </TableContainer>
-          )}
-      </Container>
-  );
+            //                           <IconButton component={Link} sx={{ mr: 3 }} to={`/teachers/${teacher.id}/delete`} >
+            //                               <DeleteForeverIcon sx={{ color: "red" }} />
+            //                           </IconButton>
+            //                       </TableCell>
+            //                   </TableRow>
+            //               ))}
+            //           </TableBody>
+            //       </Table>
+            //   </TableContainer>
+            <TableContainer component={Paper}>
+  <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    <TableHead>
+      <TableRow>
+        <TableCell>#</TableCell>
+        <TableCell align="center">
+          <Button onClick={() => handleSort("name")}>Name</Button>
+        </TableCell>
+        <TableCell align="center">
+          <Button onClick={() => handleSort("address")}>Address</Button>
+        </TableCell>
+        <TableCell align="center">
+          <Button onClick={() => handleSort("phone")}>Phone</Button>
+        </TableCell>
+        <TableCell align="center">
+          <Button onClick={() => handleSort("age")}>Age</Button>
+        </TableCell>
+        <TableCell align="center">
+          <Button onClick={() => handleSort("email")}>Email</Button>
+        </TableCell>
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      {teachers.map((teacher, index) => (
+        <TableRow key={teacher.id}>
+          <TableCell component="th" scope="row">
+            {index + 1}
+          </TableCell>
+          <TableCell align="center">{teacher.name}</TableCell>
+          <TableCell align="center">{teacher.address}</TableCell>
+          <TableCell align="center">{teacher.phone}</TableCell>
+          <TableCell align="center">{teacher.age}</TableCell>
+          <TableCell align="center">{teacher.email}</TableCell>
+          <TableCell align="center">
+            <IconButton
+              component={Link}
+              sx={{ mr: 3 }}
+              to={`/teachers/${teacher.id}/details`}
+            >
+              <Tooltip title="View teacher details" arrow>
+                <ReadMoreIcon color="primary" />
+              </Tooltip>
+            </IconButton>
+            <IconButton
+              component={Link}
+              sx={{ mr: 3 }}
+              to={`/teachers/${teacher.id}/edit`}
+            >
+              <EditIcon />
+            </IconButton>
+            <IconButton
+              component={Link}
+              sx={{ mr: 3 }}
+              to={`/teachers/${teacher.id}/delete`}
+            >
+              <DeleteForeverIcon sx={{ color: "red" }} />
+            </IconButton>
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
+</TableContainer>
+)}
+</Container>
+);
 };
+
+//           )}
+//       </Container>
+//   );
+// };
